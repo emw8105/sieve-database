ce967b8d1fd3e2e3d0d704cf8f9d7e363f80ecb7
duyenemily <duyenemily.nguyen@gmail.com>
2024-04-06 20:18:12 -0500

    Updated the database python file with new functions.

--- mysql_database.py
+++ mysql_database.py
@@ -1,12 +1,30 @@
 import mysql.connector
 
-def connect_to_sql_server():
-    database = mysql.connector.connect(
-        host = "localhost",
-        user = "root"
-    )
+class Database:
+    def __init__(self):
+        self.file_wikipedia_20160701_0 = "pagecounts-20160701-000000.txt"
+    
+    def connect_to_sql_server(self):
+        self.database_connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root"
+        )
+        print(self.database_connection)
+        self.cursor = self.database_connection.cursor()
+    
+    def create_database(self):
+        self.cursor.execute("DROP DATABASE IF EXISTS Sieve")
+        self.cursor.execute("CREATE DATABASE Sieve")
+        self.cursor.execute("USE Sieve")
 
-    print(database)
+    def close(self):
+        self.database_connection.close()
+
+
+def set_up():
+    database = Database()
+    database.connect_to_sql_server()
+    database.create_database()
     database.close()
 
-connect_to_sql_server()
+set_up()
