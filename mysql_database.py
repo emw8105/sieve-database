66485b0c1b24334040cf5b9d7d25ad7d53f65f42
duyenemily <duyenemily.nguyen@gmail.com>
2024-04-06 22:07:58 -0500

    Loaded the data from the filtered wikipedia file into the database

--- mysql_database.py
+++ mysql_database.py
@@ -17,7 +17,31 @@ class Database:
         self.cursor.execute("CREATE DATABASE sieve")
         self.cursor.execute("USE sieve")
 
+    def load_wikipedia_data(self):
+        wikipedia_table = """CREATE TABLE Wikipedia(
+                             Project_Type		VARCHAR(15)		NOT NULL,
+                             Page_Name			MEDIUMTEXT		NOT NULL,
+                             View_Count			INT				NOT NULL,
+                             Bytes_Transferred	BIGINT			NOT NULL
+                             )"""
+        self.cursor.execute(wikipedia_table)
+        
+        alter_table_query = "ALTER TABLE Wikipedia CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci"
+        self.cursor.execute(alter_table_query)
+        
+        file = open(self.file_wikipedia_20160701_0, "r", encoding="utf8")
+        next(file)
+        for line in file:   
+            line = line.strip()
+            values = line.split(' ')
+            query = "INSERT INTO Wikipedia (Project_Type, Page_Name, View_Count, Bytes_Transferred) VALUES (%s, %s, %s, %s)"
+            self.cursor.execute(query, values)
+        file.close()
+        self.database_connection.commit()
+        
+
     def close(self):
+        self.cursor.close()
         self.database_connection.close()
 
 
@@ -25,6 +49,7 @@ def set_up():
     database = Database()
     database.connect_to_sql_server()
     database.create_database()
+    database.load_wikipedia_data()
     database.close()
 
 set_up()
