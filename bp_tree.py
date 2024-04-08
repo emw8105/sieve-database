f6e28a7d0735e29a286d3b8bd58a296730999d59
duyenemily <duyenemily.nguyen@gmail.com>
2024-04-08 16:53:00 -0500

    Began node class

--- bp_tree.py
+++ bp_tree.py
@@ -1,8 +1,13 @@
 from sortedcontainers import SortedList
 
-class Bp_Tree:
+class Node:
     def __init__(self, order):
         self.order = order
-        self.leaves = SortedList()
-        self.pointers = {}
+        self.keys = SortedList()
+        self.values = SortedList()
+        self.leaf = True
+
+class Bp_Tree:
+    def __init__(self, order):
+        self.root = Node(order)
 
